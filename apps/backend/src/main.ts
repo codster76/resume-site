b09bbe556725b9df95aca5edfeec29fc7de5e76b
codster76 <codster761@gmail.com>
2022-08-18 17:52:02 +1000

    Initial commit

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,10 +8,10 @@ import * as express from 'express';
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to backend!' });
+  res.send({ message: 'Poopoo peepee' });
 });
 
-const port = process.env.port || 3333;
+const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
