ea2557167486eaf8903e1fcef173847a655ce78a
codster76 <codster761@gmail.com>
2022-08-23 06:35:37 +1000

    Forgot to switch urls back for the production build

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,10 +5,10 @@ import itemRoutes from './routes/itemRoutes';
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:4200' })); // Only allow requests from localhost:4200
-// app.use(
-//   cors({ origin: 'https://resume-site-d8i5brpo3-codster76.vercel.app/' })
-// );
+// app.use(cors({ origin: 'http://localhost:4200' })); // Only allow requests from localhost:4200
+app.use(
+  cors({ origin: 'https://resume-site-d8i5brpo3-codster76.vercel.app/' })
+);
 app.use('/api/items', itemRoutes); // /api/items is the url everything will be accessible from
 
 app.get('/', (req: any, res: any) => {
